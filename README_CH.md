2eaca5f29efdd20ca1f3a31e55f004261766fbb6
Carolina-hora <carolina22hora@gmail.com>
2023-03-17 22:46:52 +0000

    Final version, including button functioning with window.onloadgit add .

---
+++ README_CH.md
@@ -0,0 +1,5 @@
+This code was done using a combination of Javascript and HTML; the styles were mainly applied with Bootstrap.
+
+This idea behind this work is to generate a random excuse everytime the page reloads.
+
+The Javascript is quite simple, but it does provide some interactions with the user.
--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const who = ["My dad", "My husband", "The movie star", "The President"];
+  const action = ["deleted", "crashed", "erased", "destroyed"];
+  const what = ["my laptop", "the house", "the pool", "my notebook"];
+  const when = [
+    "just before class",
+    "at 4 am",
+    "at the worst possible time",
+    "last night"
+  ];
+  const randomValue = arr => {
+    return arr[Math.floor(Math.random() * arr.length)];
+  };
+  const whoRandom = randomValue(who);
+  //console.log(whoRandom);
+  const actionRandom = randomValue(action);
+  //console.log(actionRandom);
+  const whatRandom = randomValue(what);
+  //console.log(whatRandom);
+  const whenRandom = randomValue(when);
+  //console.log(whenRandom);
+  document.querySelector(
+    "#excuse"
+  ).innerHTML = `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}`;
+
+  //console.log("Hello Rigo from the console!");
 };
+
+let button = document.querySelector("#myButton");
+button.addEventListener("click", () => {
+  window.onload();
+});
