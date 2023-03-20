2c491039982d7440e4e481d92c8542159210441b
Carolina-hora <carolina22hora@gmail.com>
2023-03-20 20:05:50 +0000

    Button working properly, HTML improved using Bootstrap

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+This code was done using a combination of Javascript and HTML; the styles were mainly applied with Bootstrap.
+
+This idea behind this work is to generate a random excuse everytime the user clicks the buttom.
+
+The Javascript is quite simple, but it does provide some interactions with the user.
+
+```javascript
+npm run start
+````
+
 # Hello World with Vanilla JS
 
 Start coding a JS/HTML/CSS website in 30 seconds by opening this template in codespaces (coming soon) or [gitpod](https://gitpod.io#https://github.com/4GeeksAcademy/vanillajs-hello.git).
--- src/app.js
+++ src/app.js
@@ -5,36 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = () => {
-  //write your code here
-  const who = ["My dad", "My husband", "The movie star", "The President"];
-  const action = ["deleted", "crashed", "erased", "destroyed"];
-  const what = ["my laptop", "the house", "the pool", "my notebook"];
-  const when = [
-    "just before class",
-    "at 4 am",
-    "at the worst possible time",
-    "last night"
-  ];
-  const randomValue = arr => {
-    return arr[Math.floor(Math.random() * arr.length)];
-  };
-  const whoRandom = randomValue(who);
-  //console.log(whoRandom);
-  const actionRandom = randomValue(action);
-  //console.log(actionRandom);
-  const whatRandom = randomValue(what);
-  //console.log(whatRandom);
-  const whenRandom = randomValue(when);
-  //console.log(whenRandom);
-  document.querySelector(
-    "#excuse"
-  ).innerHTML = `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}`;
+//data
+const who = ["My dad", "My husband", "The movie star", "The President"];
+const action = ["deleted", "crashed", "erased", "destroyed"];
+const what = ["my laptop", "the house", "the pool", "my notebook"];
+const when = [
+  "just before class",
+  "at 4 am",
+  "at the worst possible time",
+  "last night"
+];
 
-  //console.log("Hello Rigo from the console!");
+//choosing a random index for each array
+const randomValue = arr => {
+  return arr[Math.floor(Math.random() * arr.length)];
 };
 
-let button = document.querySelector("#myButton");
+//Creating the arrow function that will generate the random excuse
+const excuseGenerator = () => {
+  const whoRandom = randomValue(who);
+  const actionRandom = randomValue(action);
+  const whatRandom = randomValue(what);
+  const whenRandom = randomValue(when);
+
+  let excuse = document.querySelector(".excuse");
+  excuse.innerHTML = `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}`;
+  return;
+};
+
+// attaching the function excuseGenerator to the window.onload Event
+window.onload = () => {
+  excuseGenerator();
+};
+
+//creating a button that executes the function excuseGenerator when the user clicks it
+const button = document.querySelector(".myButton");
 button.addEventListener("click", () => {
-  window.onload();
+  excuseGenerator();
 });
